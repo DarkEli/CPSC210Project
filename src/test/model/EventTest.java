884cb5f572a190a93ca1d7ebab330e199b71ed10
DarkEli0425 <elliottstupid18@gmail.com>
2023-04-01 02:47:36 -0700

    Phase 4 Test 10

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -39,7 +39,8 @@ public class EventTest {
         Event e4 = null;
         EventLog eventLog1 = new EventLog();
 
-        assertTrue(e1.getDate().equals(e2.getDate()));
+        assertFalse(e.getDate().equals(e1.getDate()));
+        assertFalse(e.equals(e1));
         assertTrue(e1.equals(e2));
         assertFalse(e1.equals(e3));
         assertFalse(e1.equals(e4));
